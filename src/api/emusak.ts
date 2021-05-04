2019a1809bc02c7c96a9c10053cebf993a45e99e
stromcon <someone@email.com>
2021-05-04 02:51:21 +0200

    Add shaders download

--- src/api/emusak.ts
+++ src/api/emusak.ts
@@ -11,13 +11,23 @@ export interface IEmusakSaves {
   [key: string]: string;
 }
 
-export const getEmusakShadersCount = async (): Promise<IEmusakShadersCount> => fetch(`${process.env.EMUSAK_URL}/api/shaders/ryujinx/count`).then(r => r.json())
+export const enum PATHS {
+  COUNT_SHADERS = '/api/shaders/ryujinx/count',
+  LIST_SAVES = '/api/saves',
+  FIRMWARE_VERSION = '/api/firmware/version',
+  PROD_KEYS = '/api/keys',
+  FIRMWARE_DOWNLOAD = '/api/firmware',
+  INFO_DOWNLOAD = '/api/shaders/ryujinx?type=info',
+  ZIP_DOWNLOAD = '/api/shaders/ryujinx?type=zip',
+}
 
-export const getEmusakSaves = async (): Promise<IEmusakSaves> => fetch(`${process.env.EMUSAK_URL}/api/saves`).then(r => r.json())
+export const getEmusakShadersCount = async (): Promise<IEmusakShadersCount> => fetch(`${process.env.EMUSAK_URL}${PATHS.COUNT_SHADERS}`).then(r => r.json())
 
-export const getEmusakFirmwareVersion = async (): Promise<string> => fetch(`${process.env.EMUSAK_URL}/api/firmware/version`).then(r => r.text()).then(v => v);
+export const getEmusakSaves = async (): Promise<IEmusakSaves> => fetch(`${process.env.EMUSAK_URL}${PATHS.LIST_SAVES}`).then(r => r.json())
 
-export const getEmusakProdKeys = async (): Promise<string> => fetch(`${process.env.EMUSAK_URL}/api/keys`).then(r => r.text()).then(v => v);
+export const getEmusakFirmwareVersion = async (): Promise<string> => fetch(`${process.env.EMUSAK_URL}${PATHS.FIRMWARE_VERSION}`).then(r => r.text()).then(v => v);
+
+export const getEmusakProdKeys = async (): Promise<string> => fetch(`${process.env.EMUSAK_URL}${PATHS.PROD_KEYS}`).then(r => r.text()).then(v => v);
 
 export const downloadFirmwareWithProgress = async (progressCallback: Function): Promise<void> => {
   const filename = 'firmware.zip';
@@ -25,7 +35,7 @@ export const downloadFirmwareWithProgress = async (progressCallback: Function): 
   await downloadFileWithProgress({
     progressCallback,
     filePath: firmwarePath,
-    url: '/api/firmware'
+    url: PATHS.FIRMWARE_DOWNLOAD
   });
   await Swal.fire('Job done !', 'EmuSAK will now open the downloaded firmware location. Go to Ryujinx ⇾ tools ⇾ install firmware ⇾ "Install Firmware from xci or zip" and select downloaded file')
   electron.shell.showItemInFolder(firmwarePath);
