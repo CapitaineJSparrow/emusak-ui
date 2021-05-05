a35667c71806de5da93ee726bb20d9dfe6969dff
stromcon <someone@email.com>
2021-05-05 05:59:04 +0200

    First try to auto update application

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { app, BrowserWindow } from 'electron';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
+require('update-electron-app')()
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
