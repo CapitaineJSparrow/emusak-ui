bfbf31753a0029d297a4485d3594a4e67b08231d
stromcon <6690149+stromcon@users.noreply.github.com>
2021-05-24 13:48:23 -0700

    Make share shaders feature work on linux

--- src/ui/page/ryujinx/gamelist/ShadersList.tsx
+++ src/ui/page/ryujinx/gamelist/ShadersList.tsx
@@ -120,7 +120,6 @@ export default ({
                     &nbsp;
                     &nbsp;
                     <Button
-                      disabled={!localShadersCount || (localShadersCount <= (emusakCount + threshold))}
                       onClick={() => onShareShaderButtonClick(config, titleId, name, localShadersCount, emusakCount)}
                       variant="contained"
                       color="primary"
