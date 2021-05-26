5c5cc5f080c37f49e6367465faefe6fe9f7e6357
stromcon <6690149+stromcon@users.noreply.github.com>
2021-05-27 00:57:15 +0200

    Update changelog

--- src/ui/changelog.tsx
+++ src/ui/changelog.tsx
@@ -35,11 +35,7 @@ const Changelog = () => {
         <h1 style={{ textAlign: 'center' }}>What's new ? v{version}</h1>
         <br />
         <ul style={{ marginLeft: 20 }}>
-          <li>Some debug & add game version when you share shaders</li>
-          <li>Added a loader when you share shaders and upload is pending</li>
-          <li>Added a threshold indicator to explain when you can share shaders</li>
-          <li>Make the new share shaders feature compatible for our linux users</li>
-          <li>When you click on the "Share Shaders" button, Emusak will now ask to open Ryujinx and open the game to read Ryujinx logs and be sure no errors occurred. This is designed to reduce workload on our side, because validation submissions is very time consuming</li>
+          <li>Mods support is finally here ! For now there is only mods from <a href="#" onClick={() => electron.shell.openExternal("https://github.com/theboy181/switch-ptchtxt-mods")}>TheBoy181</a></li>
         </ul>
         <br/>
         <p>
