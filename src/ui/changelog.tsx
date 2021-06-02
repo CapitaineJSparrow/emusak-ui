51c56af6f8f2d8ef332596fda51798b883e5b1ca
stromcon <6690149+stromcon@users.noreply.github.com>
2021-06-03 00:13:57 +0200

    1.0.70

--- src/ui/changelog.tsx
+++ src/ui/changelog.tsx
@@ -35,6 +35,7 @@ const Changelog = () => {
         <h1 style={{ textAlign: 'center' }}>What's new ? v{version}</h1>
         <br />
         <ul style={{ marginLeft: 20 }}>
+          <li>Added a progress indicator while downloading mods (some texture packs are heavy)</li>
           <li>Fixed a bug preventing users sharing their shaders from mailine configuration if logs were disabled on mainline build while logs are disabled and using Ryujinx LDN without portable mode (thanks PHAT for reporting)</li>
           <li>Uploaded tons of new mods</li>
           <li>Add romFS mods support</li>
