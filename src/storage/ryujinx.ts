937a7014a7bf29b3e90ae3facdca1bc7d0e446b0
stromcon <6690149+stromcon@users.noreply.github.com>
2021-07-06 04:51:34 +0200

    Minor refactoring

--- src/storage/ryujinx.ts
+++ src/storage/ryujinx.ts
@@ -2,10 +2,10 @@ import { IEmulatorStorageInterface, IRyujinxConfig } from "../types";
 import { isRyujinxPortableMode } from "../service/Ryujinx/system";
 
 class RyujinxModel implements IEmulatorStorageInterface {
-  static readonly LS_DIRECTORIES = "ryu-dir"
+  public readonly LS_DIRECTORIES_KEY = "ryu-dir"
 
-  async getDirectories(): Promise<IRyujinxConfig[]> {
-    const d = localStorage.getItem(RyujinxModel.LS_DIRECTORIES);
+  public async getDirectories(): Promise<IRyujinxConfig[]> {
+    const d = localStorage.getItem(this.LS_DIRECTORIES_KEY);
     let directories = d ? JSON.parse(d) : [];
 
     // Check dynamically if directories are portable
@@ -17,19 +17,19 @@ class RyujinxModel implements IEmulatorStorageInterface {
     return directories;
   }
 
-  async addDirectory(conf: IRyujinxConfig): Promise<void> {
+  public async addDirectory(conf: IRyujinxConfig): Promise<void> {
     const directories = await this.getDirectories();
 
     if (!directories.map(d => d.path).includes(conf.path)) {
       directories.push(conf);
-      localStorage.setItem(RyujinxModel.LS_DIRECTORIES, JSON.stringify(directories));
+      localStorage.setItem(this.LS_DIRECTORIES_KEY, JSON.stringify(directories));
     }
   }
 
-  async deleteDirectory(conf: IRyujinxConfig): Promise<boolean> {
+  public async deleteDirectory(conf: IRyujinxConfig): Promise<boolean> {
     let directories = await this.getDirectories();
     directories = directories.filter(d => d.path !== conf.path);
-    localStorage.setItem(RyujinxModel.LS_DIRECTORIES, JSON.stringify(directories));
+    localStorage.setItem(this.LS_DIRECTORIES_KEY, JSON.stringify(directories));
 
     return true;
   }
