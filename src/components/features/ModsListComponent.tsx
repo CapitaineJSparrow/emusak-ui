5b3dd07167f95196c29aaad393b8ee7081da79f3
stromcon <6690149+stromcon@users.noreply.github.com>
2021-07-12 12:25:38 +0200

    Init mods tab

--- src/components/features/ModsListComponent.tsx
+++ src/components/features/ModsListComponent.tsx
@@ -16,48 +16,45 @@ interface IModsListComponentProps {
   games: IEmusakGame[];
 }
 
-const ModsListComponent = ({ games }: IModsListComponentProps) => {
-
-  return (
-    <TableContainer component={Paper}>
-      <Table className="table-sticky" aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Games ({games.length})</TableCell>
-            <TableCell style={{ width: 205 }}>Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {
-            games
-              .map(g => {
-                return (
-                  <TableRow key={g.id}>
-                    <TableCell>
-                      <span dangerouslySetInnerHTML={{ __html: g.name }}/>
-                      <br/>
-                      <span><small>{g.id.toUpperCase()}</small></span>
-                    </TableCell>
-                    <TableCell>
-                      <Box display="flex" justifyContent="space-between">
-                        <Button
-                          fullWidth
-                          variant="contained"
-                          color="primary"
-                          disabled={true}
-                        >
-                          Download mods
-                        </Button>
-                      </Box>
-                    </TableCell>
-                  </TableRow>
-                );
-              })
-          }
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
-};
+const ModsListComponent = ({ games }: IModsListComponentProps) => (
+  <TableContainer component={Paper}>
+    <Table className="table-sticky" aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell>Games ({games.length})</TableCell>
+          <TableCell style={{ width: 205 }}>Actions</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {
+          games
+            .map(g => {
+              return (
+                <TableRow key={g.id}>
+                  <TableCell>
+                    <span dangerouslySetInnerHTML={{ __html: g.name }}/>
+                    <br/>
+                    <span><small>{g.id.toUpperCase()}</small></span>
+                  </TableCell>
+                  <TableCell>
+                    <Box display="flex" justifyContent="space-between">
+                      <Button
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        disabled={true}
+                      >
+                        Download mods
+                      </Button>
+                    </Box>
+                  </TableCell>
+                </TableRow>
+              );
+            })
+        }
+      </TableBody>
+    </Table>
+  </TableContainer>
+);
 
 export default ModsListComponent;
