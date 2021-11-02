d714c5ce92fc6c4f5cfebb0586b7dfadedc880ad
CapitaineJSparrow <discordbotdupont@protonmail.com>
2021-11-02 04:17:33 +0100

    feat(listing) Scan yuzu filesystem

--- src/main/routes/systemScan.ipc.ts
+++ src/main/routes/systemScan.ipc.ts
@@ -61,7 +61,8 @@ const systemScanIpc = async (kind: EmusakEmulatorsKind, binaryPath: string): Pro
 
 const scanGamesForConfig = async (dataPath: string, emu: EmusakEmulatorsKind): Promise<EmusakEmulatorGames> => {
   if (emu === 'yuzu') {
-    throw new Error('not implemented yet');
+    const directories = await fs.readdir(path.join(dataPath, 'cache', 'game_list'), { withFileTypes: true });
+    return directories.filter(d => d.isFile()).map(d => d.name.toLowerCase().replace('.pv.txt', ''));
   }
 
   const directories = await fs.readdir(path.join(dataPath, 'games'), { withFileTypes: true });
