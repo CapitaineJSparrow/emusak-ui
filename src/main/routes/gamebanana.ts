1e28fb1beb2fd8f72aedf99936098a8922c61bdb
CapitaineJSparrow <6690149+CapitaineJSparrow@users.noreply.github.com>
2022-02-23 21:21:06 +0100

    chore: Move some strings to i18n, extract game banana mods to own component and improve error handling

--- src/main/routes/gamebanana.ts
+++ src/main/routes/gamebanana.ts
@@ -1,10 +1,11 @@
 import HttpService from "../services/HttpService";
 import fetch from "node-fetch";
 import cheerio from "cheerio";
+import { GameBananaMod } from "../../types";
 
 export type searchProps = [string];
 
-export const searchGameBana = async (...args: searchProps) => {
+export const searchGameBana = async (...args: searchProps): Promise<GameBananaMod[]> => {
   const [name] = args;
   const res = await HttpService.searchGameBana(name) as unknown as { _idRow: number }[];
 
@@ -13,12 +14,18 @@ export const searchGameBana = async (...args: searchProps) => {
   }
 
   const url = `https://gamebanana.com/mods/games/${res[0]._idRow}?mid=SubmissionsList&vl[preset]=most_dld&vl%5Border%5D=downloads`;
-  const modPageContent = await fetch(url).then(r => r.text()).catch(() => "");
+  const modPageContent = await fetch(url).then(r => r.text()).catch(() => null);
+
+  if (!modPageContent) {
+    return [];
+  }
+
   const $ = cheerio.load(modPageContent);
   const modsIdentifiers = $("recordCell[class='Identifiers']");
   const modsPreviews = $("recordCell[class='Preview']");
 
-  return modsIdentifiers.map((i, element) => {
+  const mods = modsIdentifiers.map((i, element) => {
+    if (!modsPreviews[i]) return {};
     const modElement = $(element).find("a[class='Name']");
     return {
       name: modElement.text().trim(),
@@ -26,4 +33,6 @@ export const searchGameBana = async (...args: searchProps) => {
       cover: $(modsPreviews[i]).find("img").attr("src")
     };
   });
+
+  return Object.values(mods).filter(d => d.name && d.url && d.cover) as GameBananaMod[];
 };
---
+++ src/renderer/components/GameBananaModsComponent.tsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from "react";
+import { GameBananaMod } from "../../types";
+import { ipcRenderer } from "electron";
+import { Alert, Box, Divider, Grid, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import useTranslation from "../i18n/I18nService";
+
+const Label = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  border: "1px solid black",
+  borderBottomLeftRadius: 0,
+  borderBottomRightRadius: 0,
+  alignItems: "center",
+  justifyContent: "center",
+  padding: "8px 8px",
+  color: "#FFF",
+  zIndex: 1,
+  lineHeight: 1.5,
+  height: "3em",
+  whiteSpace: "nowrap",
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  display: "block",
+  textAlign: "center"
+}));
+
+
+const GameBanaCover = styled(Box)(() => ({
+  width: "100%",
+  aspectRatio: "16 / 9",
+  backgroundSize: "contain"
+}));
+
+type Props = {
+  title?: string,
+};
+
+const GameBananaModsComponent = ({ title }: Props) => {
+  const [gameBananaMods, setGameBananaMods] = useState<GameBananaMod[]>(null);
+  const [ipcPending, setIpcPending] = useState(true);
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    if (title) {
+      ipcRenderer.invoke("search-gamebanana", title).then(d => {
+        setGameBananaMods(d || []);
+        setIpcPending(false);
+      });
+    }
+  }, [title]);
+
+  const renderModsList = () => (
+    <Box pb={3}>
+      {
+        gameBananaMods.length === 0
+          ? <Alert severity="info">{t("gamebananaNoMods")}</Alert>
+          : <Grid container spacing={2}>
+            {
+              (gameBananaMods).map(mod => (
+                <Grid key={mod.name} item xs={2}>
+                  <a style={{ textDecoration: "none", color: "#FFF" }} href={mod.url} className="no-blank-icon" target="_blank">
+                    <Label title={mod.name}>{mod.name}</Label>
+                    <GameBanaCover style={{ background: `url(${ mod.cover }) no-repeat center center` }} />
+                  </a>
+                </Grid>
+              ))
+            }
+          </Grid>
+      }
+    </Box>
+  );
+
+  return (
+    <>
+      <h3>{t("gamebananaModsTitle")}</h3>
+      <Divider />
+      <br />
+      {
+        (!ipcPending)
+          ? renderModsList()
+          : (
+            <Box pt={16} style={{ position: "relative" }}>
+              <div className="loader-container">
+                <div className="lds-ripple">
+                  <div></div>
+                  <div></div>
+                </div>
+                <Typography variant="h6">{t("gameBananaLoading")}</Typography>
+              </div>
+            </Box>
+          )
+      }
+    </>
+  );
+};
+
+export default GameBananaModsComponent;
