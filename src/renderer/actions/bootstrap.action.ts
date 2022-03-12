9955b2ea4107af864956ecae5bd5add97899e03a
CapitaineJSparrow <6690149+CapitaineJSparrow@users.noreply.github.com>
2022-03-12 16:44:38 +0100

    fix: Fin an issue where eshop date where not updated once a day

--- src/renderer/actions/bootstrap.action.ts
+++ src/renderer/actions/bootstrap.action.ts
@@ -17,7 +17,7 @@ interface IBootstrap {
   currentVersion?: string;
 }
 
-const lastEshopUpdate = localStorage.getItem(LS_KEYS.ESHOP_UPDATE) ? +localStorage.getItem(LS_KEYS.ESHOP_UPDATE) : Infinity;
+const lastEshopUpdate = localStorage.getItem(LS_KEYS.ESHOP_UPDATE) ? +localStorage.getItem(LS_KEYS.ESHOP_UPDATE) : null;
 
 const createBootstrapSlice = (set: SetState<IBootstrap>, get: GetState<IDownloadManager>): IBootstrap => ({
   isAppInitialized: false,
@@ -37,11 +37,14 @@ const createBootstrapSlice = (set: SetState<IBootstrap>, get: GetState<IDownload
       mods
     ] = await ipcRenderer.invoke("load-components", process.env.EMUSAK_CDN);
 
-    const today = new Date();
-    const tomorrow = new Date(today);
+    const dayInMilliseconds = 1000 * 60 * 60 * 24;
+    const tomorrow = new Date();
     tomorrow.setDate(tomorrow.getDate() + 1);
+    tomorrow.setHours(0, 0, 0, 0);
 
-    if (lastEshopUpdate > (tomorrow.getTime())) {
+    const lastEshopUpdatePlus1Day = new Date(lastEshopUpdate + dayInMilliseconds).setHours(0, 0, 0, 0);
+
+    if (!lastEshopUpdate || (tomorrow.getTime() > lastEshopUpdatePlus1Day)) {
       get().upsertFileAction({
         filename: t("updatingEshop"),
         progress: Infinity,
@@ -51,7 +54,7 @@ const createBootstrapSlice = (set: SetState<IBootstrap>, get: GetState<IDownload
       ipcRenderer.invoke("update-eshop-data").then((res) => {
 
         if (res === true) {
-          localStorage.setItem(LS_KEYS.ESHOP_UPDATE, `${today.getTime()}`);
+          localStorage.setItem(LS_KEYS.ESHOP_UPDATE, `${new Date().getTime()}`);
         }
 
         get().removeFileAction(t("updatingEshop"));
