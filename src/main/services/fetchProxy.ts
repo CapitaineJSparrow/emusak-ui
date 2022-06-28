f914d8d5284ec08b0a554fccc403cb99eca0d453
CapitaineJSparrow <6690149+CapitaineJSparrow@users.noreply.github.com>
2022-06-28 20:38:38 +0200

    Update src/main/services/fetchProxy.ts

--- src/main/services/fetchProxy.ts
+++ src/main/services/fetchProxy.ts
@@ -2,9 +2,12 @@ import fetch, { RequestInfo, RequestInit } from "node-fetch";
 import httpsProxyAgent from "https-proxy-agent";
 import { SYS_SETTINGS } from "../../index";
 
-export default function fetchProxy(url: RequestInfo, init: RequestInit = {}) {
-  if (SYS_SETTINGS.proxy) {
+const fetchProxy = (url: RequestInfo, init: RequestInit = {}) => {
+  const proxy = process.env.HTTPS_PROXY || process.env.HTTP_PROXY || SYS_SETTINGS.proxy;
+  if (proxy) {
     init.agent = httpsProxyAgent(SYS_SETTINGS.proxy);
   }
   return fetch(url, init);
 }
+
+export default fetchProxy;
